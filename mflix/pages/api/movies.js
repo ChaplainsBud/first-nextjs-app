ae0b792738a8ea4f52a16a66a7237c936be45cec
Matthew Leroe <matthewleroe@Matthews-MBP.fios-router.home>
2021-03-31 20:59:33 -0400

    mflix or First-NextJS-App?

---
+++ mflix/pages/api/movies.js
@@ -0,0 +1,18 @@
+import { connectToDatabase } from "../../util/mongodb";
+
+export default async (req, res) => {
+  const { pid } = req.query
+
+  const { db } = await connectToDatabase();
+
+  const movies = await db
+    .collection("movies")
+    .find({})
+    .sort({ metacritic: -1 })
+    .limit(20)
+    .toArray();
+
+  res.json(movies);
+};
+
+// 573a1390f29313caabcd42e8
